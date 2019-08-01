2781d4b406c144308262fda1915bd9f9153b8828
Unicorn <878038254@qq.com>
2019-08-02 00:32:40 +0800

    初始化醒目

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
+// 入口文件
+// 引入需要的依赖模块(js 包 css 其他资源)
+// 创建根vue实例
 import Vue from 'vue'
 import App from './App.vue'
 
+// 产品级别提示 不开启 开发阶段
 Vue.config.productionTip = false
-
+// h 渲染app根组件
 new Vue({
   render: h => h(App)
 }).$mount('#app')
