363456c32ca67364d888567e5892b750930aec4a
Unicorn <878038254@qq.com>
2019-08-02 09:52:49 +0800

    使用element-ui和vue-router

--- src/main.js
+++ src/main.js
@@ -3,10 +3,19 @@
 // 创建根vue实例
 import Vue from 'vue'
 import App from './App.vue'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
+// @是一个路径的别名 是src路径 在vue-cli项目下有效
+// index是目录的索引文件 默认使用的索引文件 index.js index.vue index.json
+import router from '@/router'
 
 // 产品级别提示 不开启 开发阶段
 Vue.config.productionTip = false
+Vue.use(ElementUI)
+
 // h 渲染app根组件
 new Vue({
+  router,
   render: h => h(App)
 }).$mount('#app')
---
+++ src/router/index.js
@@ -0,0 +1,11 @@
+// 初始化路由对象 导入路由 给main.js使用
+import VueRouter from 'vue-router'
+import Vue from 'vue'
+
+Vue.use(VueRouter)
+
+const router = new VueRouter({
+  routes: []
+})
+// 导出
+export default router
